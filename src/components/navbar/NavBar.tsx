9fba75254fbbd6a65a6048f85229de4f96a50fa6
Hadrien Jaubert <hadrien.jaubert99@gmail.com>
2024-02-12 09:40:52 +0000

    add navigation ot main page when clikcing on logo

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -7,28 +7,41 @@ import NavLink from "@/components/navbar/NavLink";
 const Navbar = () => {
   const path = usePathname();
   let background;
-  if (path.startsWith('/independant')) {
-    background = "linear-gradient(91.53deg, rgba(185, 211, 134, 0.6) 3.67%, rgba(58, 142, 186, 0.6) 100%)"
-  } else if (path.startsWith('/entreprise')) {
-    background = "linear-gradient(91.53deg, rgba(216, 146, 192, 0.6) 3.67%, rgba(185, 211, 134, 0.6) 100%)"
+  if (path.startsWith("/independant")) {
+    background =
+      "linear-gradient(91.53deg, rgba(185, 211, 134, 0.6) 3.67%, rgba(58, 142, 186, 0.6) 100%)";
+  } else if (path.startsWith("/entreprise")) {
+    background =
+      "linear-gradient(91.53deg, rgba(216, 146, 192, 0.6) 3.67%, rgba(185, 211, 134, 0.6) 100%)";
   } else {
-    background ="linear-gradient(91.53deg, rgba(216, 146, 192, 0.6) 3.67%, rgba(58, 142, 186, 0.6) 100%)"
+    background =
+      "linear-gradient(91.53deg, rgba(216, 146, 192, 0.6) 3.67%, rgba(58, 142, 186, 0.6) 100%)";
   }
 
   return (
     <nav className="flex justify-between items-center px-10 pt-4">
-      <Image src={"/logo.svg"} alt="logo-connect" width={100} height={60} />
+      <NavLink href="/">
+        <Image src={"/logo.svg"} alt="logo-connect" width={100} height={60} />
+      </NavLink>
       {path.startsWith("/independant") && (
         <div style={{ background: "#B9D386" }} className="py-2 px-6 rounded-lg">
           <p className="text-white">Indépendant</p>
         </div>
       )}
       {path.startsWith("/entreprise") && (
-        <div style={{ background: "#D892C0" }} className="py-2 px-10 rounded-lg">
+        <div
+          style={{ background: "#D892C0" }}
+          className="py-2 px-10 rounded-lg"
+        >
           <p className="text-white">Entreprise</p>
         </div>
       )}
-      <NavLink href="/portage" className={path.startsWith('/entreprise') ? 'text-white': ""}>Société de portage</NavLink>
+      <NavLink
+        href="/portage"
+        className={path.startsWith("/entreprise") ? "text-white" : ""}
+      >
+        Société de portage
+      </NavLink>
       <NavLink href="/metiers">Nos métiers</NavLink>
       <NavLink href="/histoire">Notre histoire</NavLink>
       {path.startsWith("/independant") || path.startsWith("/entreprise") ? (
@@ -37,10 +50,7 @@ const Navbar = () => {
         <Image src="searchIcon.svg" alt="Search" width={24} height={24} />
       )}
 
-      <NavLink
-        href="/decouvrir"
-        background={background}
-      >
+      <NavLink href="/decouvrir" background={background}>
         Découvrir maintenant
       </NavLink>
       <div style={{ display: "inline-block", transform: "scale(1, 3)" }}>|</div>
